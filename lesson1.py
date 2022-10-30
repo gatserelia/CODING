09277e539376951dc20b1fd725514abba2511867
gatserelia <guramikog@gmail.com>
2022-10-30 16:56:43 +0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,22 @@
+
+name ="Guram"
+surname ="Gatserelia"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "guram" არის ცვლადის მნიშვნელობა
+#print(name)
+#print(surname)
+#პრინტს გამოაქვს ობიექტი ეკრანზე
+
+age="19"
+
+
+
+name="Guram" #ეს არის str(string) ტიპის ცვლადი
+age=19 #ეს არის int(integer) მთელი რიცხვი
+height=187.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი"
+knows_programming=True
+is_ugly=False #snakecase წერის სტილი სტანდარტულად 
+print(name+" "+surname)
+print(name+" "+str(age))

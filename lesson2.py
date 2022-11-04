b71dec590483466b95f5878ac88140716e8e1753
gatserelia <116829045+gatserelia@users.noreply.github.com>
2022-11-04 20:09:49 +0400

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,38 @@
+name="Guram"
+print(len(name))   #len=lenght
+
+#my_sentence="""ჯერ ჯერობით ყველაფერი გასაგებია,საკმაოდ საინტერესოც და ველოდები
+# როდის გავიგებ მეტს"""
+
+#print(my_sentence)
+
+#knows_programming=True
+
+#if 10>5:
+     #print("hello")
+
+#tab ის საშვალებით მოხდა ინდენტაცია 
+
+full_name="Guram Gatserelia"
+# print(len(full_name))
+
+#ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად
+# print(full_name[0])
+#position=index
+
+# print("G" not in full_name)
+# print(full_name[:9:3])  #start:finnish:step
+# print(full_name [2::2])
+
+# print(full_name[-10:-2:2]) #ბოლო ასოსთვის შეგვიძლია გამოვიყენოთ -
+
+#სტრინგებს აქვს მეთოდები      1)სტრინგ.upper()    2)len(სტრინგ)
+# print(full_name.upper()) .upper-ასოებს ადიდებს .lower-ასოებს აპატარავებს
+name2="   Guro   "
+print(name2.strip()) #strip მანძილს ჭრის
+
+
+print(full_name.replace("a","#") )
+
+
+

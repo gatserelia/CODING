36d4e6a48338123481954e8cc805df68cefc9ee3
gatserelia <116829045+gatserelia@users.noreply.github.com>
2022-11-06 19:48:29 +0400

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,69 @@
+
+
+# my_name= "Guram"
+# my_surname= "Gatserelia"
+# # my_pet_name= "Mei"
+# my_age="19"
+# my_sentence= "aa {} bb {} cc {} ".format(my_name, my_surname, my_age)
+# print(my_sentence)
+
+# if "e" in my_name:
+#     print("sheicavs e-s")
+# else:
+#     print("ar sheicavs e-s")
+
+
+# input
+# output
+
+# my_name=input("enter your name: ")
+
+# print("chemi saxelia" +my_name)
+
+# my_age=19
+# my_age+=3
+# print(my_age)
+
+# my_name=input("enter your name: ") #inputit yoveltvis shemodis string
+# my_surname=input("enter your surname: ")
+# my_age=input("enter your age: ")
+# my_age+="+3=22"
+# my_sentence= "aa {} bb {} cc {} ".format(my_name, my_surname, my_age)
+# my_timetravel="gavida sami weli da var 22-is"
+# print(my_sentence)
+# print(my_timetravel)
+
+# my_multiplied=input("enter numbers: ")
+# if"my_multiplied>100"in my_multiplied:
+#     print("my_multiplied")
+# else:
+#      print("game over")
+
+
+# num1=int(input("enter num1: "))
+# num2=int(input("enter num2: "))
+
+# product=num1*num2
+
+# if product>100:
+#     print(product)
+# else:
+#     print("you lose")
+
+# print(20%6) #6*3=18 20-18=2 %-tovebs nashts
+num1=int(input("enter num1: "))
+num2=int(input("enter num2: "))
+num3=int(input("enter num3: "))
+
+
+if (num1 % 2) == 0:
+    print("you lose")
+if (num2 % 2) == 0:
+    print("game over")
+if (num3 % 2) == 0:
+    print ("caage")
+
+product=num1+num2+num3
+print(product)
+
+

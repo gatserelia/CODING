cd35d18a96614d5697f3c3762cb7fe3904563fdd
gatserelia <116829045+gatserelia@users.noreply.github.com>
2022-11-08 21:15:19 +0400

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,79 @@
+# my_name="Guram"
+
+# #საინტერაციო ცვლადი 
+# for char in my_name:
+#     print(char)
+
+# print("a"in "guram")
+
+# name1=(input("enter name1: "))
+# name2=(input("enter name2: "))
+
+# #შევქმნათ ორი საინკრემენტაციო ცვლადი
+
+# amount_of_vowels_in_name1=0
+# amount_of_vowels_in_name2=0
+# for char in name1:
+#     if char in "aeiou":
+#         amount_of_vowels_in_name1+=1
+
+# for char in name2:
+#     if char in "aeiou":
+#         amount_of_vowels_in_name2+=1
+
+
+# if amount_of_vowels_in_name1> amount_of_vowels_in_name2:
+#     print(" amount_of_vowels_in_name1 is more and it contains {} vowels".format
+#     ( amount_of_vowels_in_name1))
+
+# elif amount_of_vowels_in_name1 < amount_of_vowels_in_name2:
+#     print(" amount_of_vowels_in_name2 is more and it contains {} vowels".format
+#     ( amount_of_vowels_in_name2))
+
+# else:
+#     print("they have equal ammount of vowels")
+
+
+
+# my_sentence="dges kargi amindia"
+
+
+# amount_of_a_in_my_sentence=0
+# for char in my_sentence:
+#     if char in "a":
+#         amount_of_a_in_my_sentence+=1
+
+#  if amount_of_a_in_my_sentence
+#     print(" amount_of_a_in_my_sentence is {} vowels".format(amount_of_a_in_my_sentence))
+
+# my_sentence2="dges kargi amindia"
+# amount_of_a_in_my_sentence2=0
+# for char in my_sentence2:
+#     if char in "a":
+#         amount_of_a_in_my_sentence2+=1
+
+
+my_txt=input("enter some txt: ")
+ammount_of_a=0
+
+for char in my_txt:
+    if char=="a":
+        ammount_of_a +=1
+print('there is {} "a" in my text'.format(ammount_of_a))
+
+
+
+
+
+
+
+
+
+    
+
+    
+    
+    
+
+
+
